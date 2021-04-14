efb8d7c03b498396c4f7d9a03b5183058e96af0b
Zach <zdb1994@yahoo.com>
2021-04-13 22:26:43 -0400

    Add project files.

---
+++ OreoRPS/Hubs/GameHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace OreoRPS.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task SendMove(string user, string choice)
+        {
+            await Clients.All.SendAsync("ReceiveMove", user, choice);
+        }
+    }
+}
---
+++ OreoRPS/wwwroot/js/game.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+const connection = new signalR.HubConnectionBuilder().withUrl('/gameHub').build();
+
+const choices = [
+    document.getElementById('rockChoice'),
+    document.getElementById('paperChoice'),
+    document.getElementById('scissorsChoice'),
+]
+
+const disableChoices = (choices) => {
+    choices.forEach((choice) => choice.disabled = false);
+};
+
+const enableChoices = (choices) => {
+    choices.forEach((choice) => choice.disabled = true);
+};
+
+// Disable choices until connection is established.
+disableChoices(choices);
+
+connection.on("ReceiveMove", (user, choice) => {
+});
+
+connection.start().then(() => {
+    enableChoices(choices);
+}).catch((err) => console.error(err.toString()));
+
